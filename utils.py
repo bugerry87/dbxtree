d344f989de7092729f6576f46f30e5c6d6ecd029
Gerald Baulig <gerald.baulig@gmail.com>
2019-06-24 15:54:01 +0800

    viz all data

--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ Author: Gerald Baulig
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
+from glob import glob, iglob
 
 
 def myinput(prompt, default=None, cast=None):
@@ -34,6 +35,29 @@ def myinput(prompt, default=None, cast=None):
         else:
             return arg
     pass
+
+
+def ifile(wildcards, sort=False, recursive=True):
+    def sglob(wc):
+        if sort:
+            return sorted(glob(wc, recursive=recursive))
+        else:
+            return iglob(wc, recursive=recursive)
+
+    if isinstance(wildcards, str):
+        for wc in sglob(wildcards):
+            yield wc
+    elif isinstance(wildcards, list):
+        if sort:
+            wildcards = sorted(wildcards)
+        for wc in wildcards:
+            if any(('*?[' in c) for c in wc):
+                for c in sglob(wc):
+                    yield c
+            else:
+                yield wc
+    else:
+        raise TypeError("wildecards must be string or list.")  
 
 
 def arrange_subplots(pltc):
--- viz.py
+++ viz.py
@@ -1,70 +1,143 @@
+#!/usr/bin/env python
 """
-VISUALISE THE LIDAR DATA FROM THE KITTI DATASET
+VISUALISE THE LIDAR DATA FROM THE ANY DATASET
 
 Based on the sample code from
     https://github.com/utiasSTARS/pykitti/blob/master/demos/demo_raw.py
 And:
     http://stackoverflow.com/a/37863912
+    
+Author: Gerald Baulig
 """
 
-#Global libs
-import pykitti  # install using pip install pykitti
+#Standard libs
+from argparse import ArgumentParser
+
+#3rd-Party libs
 import numpy as np
+import pykitti  # install using pip install pykitti
 from mayavi import mlab
-from glob import iglob
 
 #Local libs
-import cluster
-
-# Load the data
-files = iglob('./2011_09_26/2011_09_26_drive_0005_sync/velodyne_points/data/*.bin')
-frames = pykitti.utils.yield_velo_scans(files)
-fig = mlab.figure(bgcolor=(0, 0, 0), size=(640, 360))
-
-frame = next(frames, [])
-if not len(frame):
-    print("Error: Empty frame!")
-    exit(1)
+from utils import *
 
 
-'''
-idx = frame[:,2] > np.mean(frame[:,2])-0.2
-frame = frame[idx]
+def init_argparse(parents=[]):
+    ''' init_argparse(parents=[]) -> parser
+    Initialize an ArgumentParser for this module.
+    
+    Args:
+        parents: A list of ArgumentParsers of other scripts, if there are any.
+        
+    Returns:
+        parser: The ArgumentParsers.
+    '''
+    parser = ArgumentParser(
+        description="Demo for embedding data via LDA",
+        parents=parents
+        )
+    
+    parser.add_argument(
+        '--data', '-X',
+        metavar='WILDCARD',
+        help="Wildcard to the LiDAR files.",
+        default='**.bin'
+        )
+    
+    parser.add_argument(
+        '--sort', '-s',
+        metavar='BOOL',
+        nargs='?',
+        type=bool,
+        help="Sort the files?",
+        default=False,
+        const=True
+        )
+    
+    return parser
 
-means = cluster.init_kmeans(frame, 100, mode='kmeans++')
-for Y, means, _, _ in cluster.kmeans(frame, means):
-    pass
-'''
 
-plot = mlab.points3d(
-    frame[:,0],
-    frame[:,1],
-    frame[:,2],
-    frame[:,3],
-    mode="point",         # How to render each point {'point', 'sphere' , 'cube' }
-    colormap='spectral',  # 'bone', 'copper',
-    scale_factor=100,     # scale of the points
-    line_width=10,        # Scale of the line, if any
-    figure=fig,
-    )
+def validate(args):
+    args.data = myinput(
+        "Wildcard to the LiDAR files.\n" + 
+        "    data ('**.bin'): ",
+        default='**.bin'
+        )
+    
+    args.sort = myinput(
+        "Sort the files?\n" + 
+        "    sort (False): ",
+        default=False
+        )
+    return args
 
-@mlab.animate(delay=10)
-def init_animation():
-    global frames, fig, plot
+
+def main(args):
+    # Load the data
+    files = ifile(args.data, args.sort)    
+    frames = pykitti.utils.yield_velo_scans(files)
+    fig = mlab.figure(bgcolor=(0, 0, 0), size=(640, 360))
+
     frame = next(frames, [])
-    while len(frame):
+    if not len(frame):
+        print("Error: Empty frame!")
+        return 1
+
+    plot = mlab.points3d(
+        frame[:,0],
+        frame[:,1],
+        frame[:,2],
+        frame[:,3],
+        mode="point",         # How to render each point {'point', 'sphere' , 'cube' }
+        colormap='spectral',  # 'bone', 'copper',
+        scale_factor=100,     # scale of the points
+        line_width=10,        # Scale of the line, if any
+        figure=fig,
+        )
+    
+    next_frame = 1
+    for frame in frames:
+        if not len(frame):
+            break
+        elif next_frame:
+            next_frame = myinput(
+                "Enter to continue or 0 for animation: ",
+                default=1,
+                cast=int
+                )
+        else:
+            break
+        
         plot.mlab_source.reset(
             x = frame[:, 0],
             y = frame[:, 1],
             z = frame[:, 2],
             scalars = frame[:, 3]
         )
-        fig.render()
-        yield
-        frame = next(frames, [])
+        fig.render() 
 
-input("Hit any key to continue!")
-animator = init_animation()
+    @mlab.animate(delay=10)
+    def animation():
+        _frame = frame
+        while len(_frame):
+            plot.mlab_source.reset(
+                x = _frame[:, 0],
+                y = _frame[:, 1],
+                z = _frame[:, 2],
+                scalars = _frame[:, 3]
+            )
+            fig.render()
+            yield
+            _frame = next(frames, [])
 
-mlab.show()
+    animator = animation()
+    mlab.show()
+    return 0
 
+if __name__ == '__main__':
+    import sys
+    parser = init_argparse()
+    args, _ = parser.parse_known_args()
+    if len(sys.argv) == 1:
+        args = validate(args)
+    exit(main(args))
