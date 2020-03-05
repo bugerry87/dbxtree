be0bbf99bc5ed1f4edd46c4bb48d895d826f8006
Gerald Baulig <gerald.baulig@gmail.com>
2020-03-05 16:06:32 +0800

    dot log

--- sim_evt_lidar.py
+++ sim_evt_lidar.py
@@ -81,32 +81,65 @@ def polarize(X, scale=(10,10)):
     return P
 
 
-def quantirize(P, t=0.9, m=0):
+def face_normals(T, normalize=True):
+    fN = np.cross(T[:,1] - T[:,0], T[:,2] - T[:,0])
+    if normalize:
+        return fN / np.linalg.norm(fN, axis=1)[:, None]
+    else:
+        return fN
+
+
+def edge_normals(fN, Ti_flat, normalize=True):
+    fN = fN.repeat(3, axis=0)
+    eN = np.zeros((Ti_flat.max()+1, 3))
+    for fn, i in zip(fN, Ti_flat):
+        eN[i] += fn
+    if normalize:
+        return eN / np.linalg.norm(eN, axis=1)[:, None]
+    else:
+        return eN
+
+
+def mask_planar(eN, fN, Ti_flat, min_dot=0.9, mask=None):
+    fN = fN.repeat(3, axis=0)
+    if mask is None:
+        mask = np.ones(Ti_flat.max()+1, dtype=bool)
+    for fn, i in zip(fN, Ti_flat):
+        if mask[i]:
+            mask[i] &= np.dot(eN[i], fn) <= min_dot
+        else:
+            pass
+    return mask
+
+
+def quantirize(P, m=1):
     k = P[0]
     p0 = P[1]
     Q = [k]
+    p0k = p0 - k
+    mag = np.linalg.norm(p0k)
+    p0k = p0k / mag
     
     for p1 in P[2:]:
         pp = p1 - p0
         ppm = np.linalg.norm(pp)
-        if ppm <= m:
-            continue
+        mag += ppm
         
-        p0 = p1
-        pk = p0 - k
-        pp = pp / ppm
-        pk = pk / np.linalg.norm(pk)
-        dot = np.dot(pp, pk)
+        p1k = p1 - k
+        p1k = p1k / np.linalg.norm(p1k)
+        dot = np.dot(p0k, p1k)
         
-        if dot >= t:
-            #reject point
+        if dot < 1 - np.exp(-mag/m):
+            #new keypoint detected
+            k = p0
             p0 = p1
-            continue
-        
-        #else new line
-        k = p0
-        p0 = p1
-        Q.append(k)
+            p0k = pp / ppm
+            mag = ppm
+            Q.append(k)
+        else:
+            #update
+            p0 = p1
+            p0k = p1k
     return np.array(Q)
 
 
@@ -121,26 +154,81 @@ def main(args):
         if not len(X):
             break
         
-        print("Input size:", X.shape)
+        m = 1
+        while m > 0:
+            print("Input size:", X.shape)
+            print("Org plot...")
+            Y = np.arange(X.shape[0])
+            plot = viz.lines(X, Y, fig, None)
+            
+            m = myinput(
+                    "Set the magnitude: ",
+                    cast=float,
+                    default=False
+                )
+            
+            if m <= 0:
+                break
         
-        print("Org plot...")
-        Y = np.arange(X.shape[0])
-        plot = viz.lines(X, Y, fig, None)
-        if input():
+            print("Magnitude:", m)
+            Q = quantirize(X, m)
+            Y = np.arange(Q.shape[0])
+            
+            viz.lines(Q, Y, fig, plot)
+            print("Output size:", Q.shape)
+            if input():
+                break
+            viz.clear_figure(fig)
+            
+            print("Plot polar...")
+            P = polarize(Q)
+            viz.vertices(P[:,(1,0,2)], P[:,3], fig, None)
+            if input():
+                break
+            viz.clear_figure(fig)
+            
+            print("Generate Surface...")
+            mesh = Delaunay(P[:,(0,2)])
+            Ti = mesh.simplices
+            viz.mesh(P[:,(2,0,1)], Ti, None, fig, None)
+            if input():
+                break
+            viz.clear_figure(fig)
+            
+            print("Remove planars...")
+            fN = face_normals(Q[Ti,:3])
+            eN = edge_normals(fN, Ti.flatten())
+            Mask = mask_planar(eN, fN, Ti.flatten(), 0.9)
+            P = P[Mask]
+            Q = Q[Mask]
+            mesh = Delaunay(P[:,(0,2)])
+            Ti = mesh.simplices
+            viz.mesh(Q, Ti, None, fig, None)
+            print("New size:", Q.shape)
+            if input():
+                break
+            viz.clear_figure(fig)
+            
+            print("Remove Narrow Z-faces...")
+            fN = face_normals(P[Ti,:3])
+            Mask = np.abs(fN[:,1]) > 0.1
+            Ti = Ti[Mask]
+            viz.mesh(P[:,(2,0,1)], Ti, None, fig, None)
+            print("Final size:", np.unique(Ti.flatten()).shape)
+            if input():
+                break
+            viz.clear_figure(fig)
+            
+            print("Unfold View...")
+            viz.mesh(Q, Ti, None, fig, None)
+            
+            if input():
+                break
+            viz.clear_figure(fig)
+            
+        viz.clear_figure(fig)  
+        if m < 0:
             break
-        #viz.clear_figure(fig)
-        
-        print("Plot quantirized...")
-        Q = quantirize(X, 0.9, 0.05)
-        Y = np.arange(Q.shape[0])
-        
-        viz.lines(Q, Y, fig, plot)
-        print("Output size:", Q.shape)
-        if input():
-            break
-        viz.clear_figure(fig)
-        
-        break
     return 0
 
 if __name__ == '__main__':
