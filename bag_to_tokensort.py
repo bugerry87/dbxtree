f256bf782615d393b5cf64e131db3d8cad56af60
bugerry87 <gerald.baulig@t-online.de>
2020-08-13 16:24:25 +0800

    err check

--- bag_to_tokensort.py
+++ bag_to_tokensort.py
@@ -63,32 +63,54 @@ def featurize(X):
 	return tokensort.featurize(X)
 
 
-def odom_to_numpy(bag, topic, d=180, b=8):
+def odom_to_numpy(bag, topic, d=180, b=3, rec_err=None):
 	abs_pos = np.zeros(3)
 	abs_ori = R.from_rotvec((0,0,0))
-
+	n = float(np.iinfo(np.int8).max)
+	b = int(b) * 2 + 1
+	
+	if rec_err is not None:
+		rec_err['seq'] = []
+		rec_err['pos_err'] = []
+		rec_err['ori_err'] = []
+		cum_pos = np.zeros(3)
+		cum_ori = R.from_rotvec((0,0,0))
+	
 	for _, odom, _ in rosbag.Bag(bag).read_messages(topics=[topic]):
 		p = odom.pose.pose.position
 		p = (p.x, p.y, p.z)
 		delta_p = p - abs_pos
-		delta_p = np.round(delta_p * 100).astype(np.int8)
+		delta_p = np.round(100 * delta_p**(1.0/b)).astype(np.int8)
 		abs_pos[:] = p
 		
 		o = odom.pose.pose.orientation
 		o = R.from_quat((o.x, o.y, o.z, o.w))
 		delta_o = (o * abs_ori.inv()).as_euler('zyx', degrees=True)
-		delta_o = np.clip(delta_o, -d, d) 
-		a = np.where(delta_o >= 0, 127.0/d, -127.0/d)
-		delta_o = a * (d**b - (d - np.abs(delta_o))**b)**(1.0/b)
+		delta_o = np.clip(delta_o, -d, d)
+		delta_o = n * (delta_o/d)**(1.0/b)
 		delta_o = np.round(delta_o).astype(np.int8)
 		abs_ori = o
 		
 		idx = np.array(odom.header.seq, dtype=np.uint16)
 		idx = np.ndarray(2, np.int8, buffer=idx)
 		
-		print("Seq [{:>5}] ".format(odom.header.seq),
-			"Pos {:<16}".format(delta_p), 
-			"Ori", delta_o)
+		if rec_err is not None:
+			cum_pos += (delta_p/100.0)**b
+			pos_err = abs_pos - cum_pos
+			
+			ori_err = d * (delta_o/n)**b
+			cum_ori *= R.from_euler('zyx', ori_err, degrees=True)
+			ori_err = (cum_ori * abs_ori.inv()).as_euler('zyx', degrees=True)
+			
+			rec_err['seq'].append(odom.header.seq)
+			rec_err['pos_err'].append(pos_err)
+			rec_err['ori_err'].append(ori_err)
+			
+			print("\nSeq [{:>5}] ".format(odom.header.seq),
+				"\nPos {:<16}".format(delta_p),
+				"Error", pos_err,
+				"\nOri {:<16}".format(delta_o),
+				"Error", ori_err)
 		yield np.hstack((delta_p, delta_o, idx))
 
 
@@ -165,6 +187,20 @@ if __name__ == '__main__':
 			default=1024
 			)
 		
+		parser.add_argument(
+			'--ang_range', '-d',
+			metavar='FLOAT',
+			type=float,
+			default=180.0
+			)
+		
+		parser.add_argument(
+			'--ang_scale', '-b',
+			metavar='INT',
+			type=int,
+			default=3
+			)
+		
 		return parser
 	
 	
@@ -178,11 +214,19 @@ if __name__ == '__main__':
 		print("\nLoad data: {} - topic: {}".format(args.bag, args.odom))
 		print("--- Convert Odometry ---")
 		
+		rec_err = {}
 		fn = os.path.join(args.output_dir, 'odom.bin')
-		with open(fn, 'wb') as fid:
-			X = np.array([x for x in odom_to_numpy(args.bag, args.odom)])
-			X = tokensort.encode(X)
-			X.tofile(fn)
+		X = odom_to_numpy(args.bag, args.odom, args.ang_range, args.ang_scale, rec_err)
+		tokensort.encode(np.array([x for x in X])).tofile(fn)
+		
+		import matplotlib.pyplot as plt
+		seq = rec_err['seq']
+		pos_err = np.array(rec_err['pos_err']).T
+		for err, label in zip(pos_err, 'xyz'):
+			plt.plot(seq, err, label=label)
+		plt.legend()
+		plt.show()
+		exit()
 			
 	if args.points:
 		print("\nLoad data: {} - topic: {}".format(args.bag, args.points))
