98491f5332eeaae64a6c661aae59697bf876579d
Gerald Baulig <gerald.baulig@gmail.com>
2020-11-09 14:58:05 +0800

    bitstream fixed

--- mhdm/dynamictree.py
+++ mhdm/dynamictree.py
@@ -17,7 +17,6 @@ def encode(X,
 	output=None,
 	breadth_first=False,
 	payload=False,
-	features=False,
 	**kwargs
 	):
 	"""
@@ -26,48 +25,43 @@ def encode(X,
 	tree_depth = tree_depth if tree_depth else np.iinfo(X.dtype).bits
 	flags = BitBuffer(output + '.flg.bin', 'wb')
 	stack_size = 0
-	msg = "Layer: {:>2}, BranchFlag: {:>16}, StackSize: {:>10}"
+	msg = "Layer: {:>2}, BranchFlag: {:>16}, StackSize: {:>10}, Points: {:>10}"
+	local = Prototype(
+		points = 0
+		)
 	
 	if payload is True:
 		payload = BitBuffer(output + '.pyl.bin', 'wb') if output else BitBuffer()
 	elif not isinstance(payload, BitBuffer):
 		payload = False
-	
-	if features is True:
-		features = np.zeros(len(X), dtype=object)
-	else:
-		features = False
 
-	def expand(X, layer, tail, Xi):
+	def expand(X, layer, tail):
 		flag = 0
 		dim = dims[layer] if layer < len(dims) else dims[-1]
 		fbit = 1<<dim
 		mask = (1<<dim)-1
 		
-		if len(X) == 0 or dim == 0 or tail == 0:
+		if dim == 0:
 			pass
 		elif payload and len(X) == 1:
 			payload.write(int(X), tail, soft_flush=True)
-			if features is not False:
-				features[Xi] <<= tail
-				features[Xi] = X
+			local.points += 1
 		else:
 			for t in range(fbit):
-				m = X & mask == t
+				m = (X & mask) == t
 				if np.any(m):
-					yield expand(X[m]>>dim, layer+1, max(0, tail-dim), False if Xi is False else Xi[m])
 					flag |= 1<<t
-			if features is not False:
-				features[Xi] <<= fbit
-				features[Xi] |= flag
+					if tail > dim:
+						yield expand(X[m]>>dim, layer+1, max(tail - dim, 0))
+					else:
+						local.points += 1
 		
 		flags.write(flag, fbit, soft_flush=True)
 		if log.verbose:
-			log(msg.format(layer, hex(flag)[2:], stack_size), end='\r', flush=True)
+			log(msg.format(layer, hex(flag)[2:], stack_size, local.points), end='\r', flush=True)
 		pass
 	
-	Xi = False if features is False else np.arange(len(X))
-	nodes = deque(expand(X, 0, tree_depth, Xi))
+	nodes = deque(expand(X, 0, tree_depth))
 	while nodes:
 		node = nodes.popleft() if breadth_first else nodes.pop()
 		nodes.extend(node)
@@ -76,8 +70,6 @@ def encode(X,
 	flags.close()
 	if payload:
 		payload.close()
-	if features is not False:
-		np.save(output + '.feat.npy', features)
 	return flags, payload
 
 
@@ -101,27 +93,30 @@ def decode(Y, num_points,
 	tree_depth = tree_depth if tree_depth else np.iinfo(qtype).bits
 	msg = "Layer: {:>2}, BranchFlag: {:>16}, Points: {:>10}, Done: {:>3.2f}%"
 	X = np.zeros(num_points, dtype=qtype)
-	Xi = iter(range(len(X)))
 	local = Prototype(
 		points = 0
 		)
 	
 	def expand(x, layer, pos):
-		tail = tree_depth-pos
+		tail = max(tree_depth - pos, 0)
 		dim = dims[layer] if layer < len(dims) else dims[-1]
 		fbit = 1<<dim
-		flag = Y.read(fbit) if tail > 0 else 0
+		flag = Y.read(fbit)
 		
 		if flag == 0:
-			if payload and tail:
+			if payload:
 				x |= payload.read(tail) << pos
-			xi = next(Xi)
-			X[xi] = x
-			local.points = xi+1
+			X[local.points] = x
+			local.points += 1
 		else:
-			for token in range(fbit):
-				if flag & 1<<token:
-					yield expand(x | token<<pos, layer+1, pos+dim)
+			for t in range(fbit):
+				if flag & 1<<t:
+					if tail > dim:
+						yield expand(x | t<<pos, layer+1, pos+dim)
+					else:
+						X[local.points] = x | t<<pos
+						local.points += 1
+			pass
 		
 		if log.verbose:
 			progress = 100.0 * local.points / len(X)
