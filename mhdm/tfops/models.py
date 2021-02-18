4a58bec3e221b63ec43e732e992588234f2dc1f5
bugerry87 <gerald.baulig@gmail.com>
2021-02-18 23:03:43 +0800

    offset should be -1

--- mhdm/tfops/models.py
+++ mhdm/tfops/models.py
@@ -331,7 +331,7 @@ class NbitTreeProbEncoder(Model):
 			cdf = tf.cast(cdf * float(1<<16), tf.int32)
 			cdf = tf.pad(cdf, [(0,0),(1,0)])
 
-			offset = tf.ones_like(flags, dtype=tf.int32)
+			offset = -tf.ones_like(flags, dtype=tf.int32)
 			index = range_like(flags, dtype=tf.int32)
 			cdf_size = tf.zeros_like(flags, dtype=tf.int32) + cdf.shape[-1]
 			
