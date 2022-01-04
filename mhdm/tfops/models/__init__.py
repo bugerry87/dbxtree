78b44fbbfd427e7cd796b42865d0df0b32eb71ff
Gerald Baulig <gerald.baulig@gmail.com>
2022-01-04 14:38:21 +0800

    norm with gradiants

--- mhdm/tfops/models/__init__.py
+++ mhdm/tfops/models/__init__.py
@@ -3,8 +3,8 @@ import tensorflow as tf
 
 @tf.function
 def normalize(X):
-	n = tf.stop_gradient(X)
-	n = tf.abs(n)
+	#n = tf.stop_gradient(X)
+	n = tf.abs(X)
 	n = tf.math.reduce_max(n, axis=-1, keepdims=True)
 	X = tf.math.divide_no_nan(X, n)
 	return X
