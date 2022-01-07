5566fb3861cb0688634b4872739ef611f99f2ed9
Gerald Baulig <gerald.baulig@gmail.com>
2022-01-07 14:01:30 +0800

    pivots w/o exp

--- mhdm/tfops/models/__init__.py
+++ mhdm/tfops/models/__init__.py
@@ -3,7 +3,6 @@ import tensorflow as tf
 
 @tf.function
 def normalize(X):
-	#n = tf.stop_gradient(X)
 	n = tf.abs(X)
 	n = tf.math.reduce_max(n, axis=-1, keepdims=True)
 	X = tf.math.divide_no_nan(X, n)
